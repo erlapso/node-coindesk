99eb19d69504f54cb26f1d08902eba6df3de318d
LapsuS <vittorio.banfi@gmail.com>
2014-02-12 20:16:21 +0100

    Example updated

--- example.js
+++ example.js
@@ -4,7 +4,8 @@ coindesk.currentPrice(null,function(data){
   console.log("Prices:");
   console.log(data);
 });
-var end_date = new Date()
+var start_date = new Date();
+var end_date = new Date();
 end_date.setDate(end_date.getDate() - 60);
 coindesk.historical({start_date: new Date(), end_date: end_date }, null, function(data) {
   console.log(data);
